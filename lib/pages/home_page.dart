42c7b93ffbf325297f6f9b9c51c4800a3fc1bbc4
Raja Ram <mr.raj00111777@gmail.com>
2022-09-26 21:29:49 +0530

    bloc builder in action

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_tutorial/bloc/internet_bloc/internet_bloc.dart';
+import 'package:flutter_bloc_tutorial/bloc/internet_bloc/internet_state.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('Loading...'),
+        child: BlocBuilder<InternetBloc, InternetState>(
+          builder: (context, state) {
+            if (state is InternetGainState) {
+              return const Text('Connected!');
+            } else if (state is InternetLostState) {
+              return const Text('Not Connected!');
+            } else {
+              return const Text('Loading...');
+            }
+          },
+        ),
       ),
     );
   }
