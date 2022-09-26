4d6d6caa19a46f73c1d5c49a04c71ff2ca161e51
Raja Ram <mr.raj00111777@gmail.com>
2022-09-26 20:54:13 +0530

    flutter bloc setup done

--- lib/bloc/internet_bloc/internet_bloc.dart
+++ lib/bloc/internet_bloc/internet_bloc.dart
@@ -1,8 +1,14 @@
+import 'dart:async';
+
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_bloc_tutorial/bloc/internet_bloc/internet_event.dart';
 import 'package:flutter_bloc_tutorial/bloc/internet_bloc/internet_state.dart';
 
 class InternetBloc extends Bloc<InternetEvent, InternetState> {
+  final Connectivity _connectivity = Connectivity();
+  StreamSubscription? connectivitySubscription;
+
   InternetBloc() : super(InternetInitialState()) {
     on<InternetLostEvent>(
       (event, emit) => InternetLostState(),
@@ -10,5 +16,20 @@ class InternetBloc extends Bloc<InternetEvent, InternetState> {
     on<InternetGainEvent>(
       (event, emit) => InternetGainState(),
     );
+    connectivitySubscription =
+        _connectivity.onConnectivityChanged.listen((result) {
+      if (result == ConnectivityResult.mobile ||
+          result == ConnectivityResult.wifi) {
+        add(InternetGainEvent());
+      } else {
+        add(InternetLostEvent());
+      }
+    });
+  }
+
+  @override
+  Future<void> close() {
+    connectivitySubscription?.cancel();
+    return super.close();
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_tutorial/bloc/internet_bloc/internet_bloc.dart';
 
 import '../pages/home_page.dart';
 
@@ -11,9 +13,12 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
+    return BlocProvider(
+      create: (context) => InternetBloc(),
+      child: const MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: HomePage(),
+      ),
     );
   }
 }
